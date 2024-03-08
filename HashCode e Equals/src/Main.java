df6fba5708f1a6b042ee29afa03205d7ee1a9d09
SamuelMax10 <samuelmax544@gmail.com>
2024-03-08 20:48:54 -0300

    add: HashCode e Equals

---
+++ HashCode e Equals/src/Main.java
@@ -0,0 +1,18 @@
+import entities.Client;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Pedro", "pedro@gmail.com");
+        Client c2 = new Client("Pedro", "pedro@gmail.com");
+
+        String s1 = "Teste";
+        String s2 = "Teste";
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
+    }
+}
