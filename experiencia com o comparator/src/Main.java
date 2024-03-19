23f1255f9d4da7a35473ce3eea66d460f8e10cf4
SamuelMax10 <samuelmax544@gmail.com>
2024-03-19 18:24:22 -0300

    Comparator objeto de classe separada

---
+++ experiencia com o comparator/src/Main.java
@@ -0,0 +1,24 @@
+import entities.Product;
+import org.w3c.dom.ls.LSInput;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort(new MyComparator());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
