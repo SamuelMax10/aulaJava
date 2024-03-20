75fb72dbab4d8b97a2aef4c17994d72ef390bda8
SamuelMax10 <samuelmax544@gmail.com>
2024-03-20 18:01:56 -0300

    add: Consumer - Implementação da interface

---
+++ Consumer/src/Main.java
@@ -0,0 +1,24 @@
+import Util.PriceUpdate;
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+    }
+}
