9d8a325bc3bc1f2f85b8f2f7638f3d0ce96f2b50
SamuelMax10 <samuelmax544@gmail.com>
2024-03-20 17:26:40 -0300

    add: Referencia com método não estático

--- Predicate/src/entities/Product.java
+++ Predicate/src/entities/Product.java
@@ -33,6 +33,11 @@ public class Product {
         return p.getPrice() >= 100.00;
     }
 
+    public boolean nonStaticProductPredicate() {
+        return price >= 100.00;
+    }
+
+
     public String toString() {
         return name + ", " + String.format("%.2f", price);
     }
