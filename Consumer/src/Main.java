bdac1d8a01bc43c349e033d505373775c5571f21
SamuelMax10 <samuelmax544@gmail.com>
2024-03-27 23:16:40 -0300

    add: Stream - exemplo pratico

--- Consumer/src/Main.java
+++ Consumer/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
-        
+
         list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 
         list.forEach(System.out::println);
