7945a9bd050a16652a4fa6ecfee8e666d3e973e7
SamuelMax10 <samuelmax544@gmail.com>
2024-04-09 19:44:12 -0300

    add: Inserir dados.

---
+++ jdbc3/src/application/Main.java
@@ -0,0 +1,67 @@
+package application;
+
+import db.DB;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        System.out.print("Insira o nome do funcionario: ");
+        String nome = sc.nextLine();
+        System.out.print("Insira o E-mail: ");
+        String email = sc.nextLine();
+        System.out.print("Insira a data de aniversario: ");
+        LocalDate date = LocalDate.parse(sc.next(), dtf);
+        System.out.print("Insira o salario base: ");
+        double baseSalario = sc.nextDouble();
+        System.out.print("Insira o departamento: ");
+        int departamento = sc.nextInt();
+
+        try {
+            con = DB.getConexao();
+
+            ps = con.prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentID)"
+                            + "VALUES "
+                            + "(?,?,?,?,?)"
+                            + Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, nome);
+            ps.setString(2, email);
+            ps.setDate(3, Date.valueOf(date));
+            ps.setDouble(4, baseSalario);
+            ps.setInt(5, departamento);
+
+            int linhasAlterada = ps.executeUpdate();
+
+//            if (linhasAlterada > 0) {
+//                ResultSet rs = ps.getGeneratedKeys();
+//                while (rs.next()) {
+//                    int id = rs.getInt(1);
+//                    System.out.println("Pronto! ID = " + id);
+//                }
+//
+//            } else {
+//                System.out.println("Nenhuma linha foi alterada. ");
+//            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        DB.fechaStatement(ps);
+        DB.fechaConexao();
+    }
+}
