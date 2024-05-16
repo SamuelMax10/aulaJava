34a03ac91c75fae4f6d7ae78dce43b9d708dcaaa
SamuelMax10 <samuelmax544@gmail.com>
2024-05-16 17:22:34 -0300

    feat: Created mapping with jpa

---
+++ aulajpa/aulajpa/src/main/java/com/iniciantejpa/aulajpa/AulajpaApplication.java
@@ -0,0 +1,15 @@
+package com.iniciantejpa.aulajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class AulajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulajpaApplication.class, args);
+	}
+
+}
